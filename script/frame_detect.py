7bf7b83e656a289698ab54112e230a779bf8161a
Jenish Patel <jpatel102005@gmail.com>
2025-05-29 00:16:35 -0400

    final

--- script/frame_detect.py
+++ script/frame_detect.py
@@ -17,7 +17,7 @@ class FaceDetect(Node):
     def __init__(self):
         super().__init__('face_detect_node')
         self.bridge = CvBridge()
-        self.cascade_path = "/home/jenish/catkin_ws/src/vision_pkg/vision_pkg/cascade/haarcascade_frontalface_default.xml"
+        self.cascade_path = "/home/jpat/facedetectapril/script/haarcascade_frontalface_default.xml"
         self.face_cascade = cv2.CascadeClassifier(self.cascade_path)
         if self.face_cascade.empty():
             print("Error loading cascade file")
