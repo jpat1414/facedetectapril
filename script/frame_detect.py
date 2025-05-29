282fe32fe6b98b5cfe2551188322dfa5a2d7add4
Jenish Patel <jpatel102005@gmail.com>
2025-05-28 23:26:20 -0400

    final

--- script/frame_detect.py
+++ script/frame_detect.py
@@ -3,39 +3,61 @@ Created on 5-23-2025
 @author: Jenish Patel
 
 """
-
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import sys
 import os
 import argparse
 
+class FaceDetect(Node):
+    def __init__(self):
+        super().__init__('face_detect_node')
+        self.bridge = CvBridge()
+        self.cascade_path = "/home/jenish/catkin_ws/src/vision_pkg/vision_pkg/cascade/haarcascade_frontalface_default.xml"
+        self.face_cascade = cv2.CascadeClassifier(self.cascade_path)
+        if self.face_cascade.empty():
+            print("Error loading cascade file")
+            sys.exit(1)
 
-record_dir = 'recording'
-if not os.path.exists(record_dir):
-    os.makedirs(record_dir)
+    def receive_image(self):    
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/color/image_raw',
+            self.frame_callback,
+            10
+        )
+        self.get_logger().info("Face detection node has been started")
+        rclpy.spin(self)
+        cv2.destroyAllWindows()
+    
+    def frame_callback(self, data):
+        frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+        if frame is None:
+            self.get_logger().error("Received an empty frame")
+            return
 
-'''
-# initializing the object tracker
-object_tracker = cv2.legacy.TrackerCSRT_create()
-webcam = cv2.VideoCapture(0)
-if not webcam.isOpened():
-    raise Exception("Could not open video device")
+        #Turns the frame into grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-ret, frame = webcam.read()
-if not ret:
-    raise Exception("Could not find video device")
+        faces = self.face_cascade.detectMultiScale(
+            gray, 
+            scaleFactor=1.1, 
+            minNeighbors=5, 
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        #Creates a bounding box around the detected faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.imshow("Face Detection", frame)
+        cv2.waitKey(1)
 
-print("object tracker initialized")
+if __name__ == '__main__':
+    rclpy.init()
+    face_detect_node = FaceDetect()
+    face_detect_node.receive_image()
 
-
-framewidth = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameheight = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-framecount = int(webcam.get(cv2.CAP_PROP_FRAME_COUNT))
-
-fourcc = cv2.VideoWriter_fourcc(*"m", "p", "4", "v")
-recording = 'recording/' + 'video.mp4'
-output = cv2.VideoWriter(recording, fourcc, 20.0, (framewidth, frameheight))
-if not output.isOpened():
-    raise Exception("Could not open video file")
-'''
